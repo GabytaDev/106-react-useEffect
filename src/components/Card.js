54709b001ef6e06c8fb7d0276c418997f8f1463f
GabytaDev <gabyta.loyola@gmail.com>
2022-02-09 15:55:25 -0300

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import "../styles/_Card.scss"
+
+const Card = ({img,nombre,especie,estado,genero})=>{
+    
+    return(
+    <div className="card">
+        <div className="container-img">
+        <img src={img}></img>
+        </div>
+        <div className="detalles-card">
+            <h3>{nombre}</h3>
+            <span>Specie: {especie}</span>  
+            <span>Status: {estado}</span>   
+            <span>Gender: {genero}</span>  
+        </div>
+    </div>
+    )
+}
+
+export default Card;
